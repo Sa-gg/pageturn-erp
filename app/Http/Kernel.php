ce5d4f24a3ffa8b700f692dbb79f854a82595c7a
Patrick Sagum <sagum.patrick.r@gmail.com>
2026-05-31 21:34:08 +0800

    fix: increase API throttle to 600/min for Render load balancer

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -38,7 +38,7 @@ class Kernel extends HttpKernel
         ],
 
         'api' => [
-            'throttle:60,1',
+            'throttle:600,1',
             'bindings',
         ],
     ];
@@ -79,3 +79,4 @@ class Kernel extends HttpKernel
         \Illuminate\Auth\Middleware\Authorize::class,
     ];
 }
+
